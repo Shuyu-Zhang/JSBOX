194c82e770eb6928487ab7b5464e86af16168cd1
Shuyu-Zhang <624269674@qq.com>
2019-01-17 09:49:06 +0800

    1

--- Baby.js
+++ Baby.js
@@ -1,8 +1,8 @@
 var wid = $device.info.screen.width
 var day_clac = (new Date()).toLocaleString()// 获取当前日期
 $ui.toast(day_clac)
-var day_dis = displaydate(day_clac)// 显示当前日期
-var DEFAULT_ = ["2018年1月13日 星期六 ","2018-01-12T16:00:00.000Z","",1,"#FF9F01","#FA8D01","#409EF6","#2C86D9"]
+//var day_dis = displaydate(day_clac)// 显示当前日期
+//var DEFAULT_ = ["2018年1月13日 星期六 ","2018-01-12T16:00:00.000Z","",1,"#FF9F01","#FA8D01","#409EF6","#2C86D9"]
 //[day_dis, day_clac, "", 0, "", "", ""]//设置默认日期
 //var SETTING_ = (typeof file == "undefined") ? JSON.parse(JSON.stringify(DEFAULT_)) : JSON.parse(file.string)//获取Setting.conf的数据
    
